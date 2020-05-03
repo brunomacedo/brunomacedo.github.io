bad92fcffa9c0ed6172fb0f3df1fff892445d969
Bruno Macedo <contato@brunomacedo.com.br>
2020-05-03 16:36:18 -0300

    feat: create jsx files componentes

---
+++ src/components/Profile/index.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Profile = () => (
+  <aside className="profile-wrapper">
+    <h1>Bruno Macedo</h1>
+    <h2>Front-end developer</h2>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas qui
+      animi quo obcaecati dolore odit autem recusandae
+    </p>
+  </aside>
+)
+
+export default Profile
