2393c041368ffad7b5887d0d750cfbe69ef46d54
Bruno Macedo <contato@brunomacedo.com.br>
2020-05-16 13:12:06 -0300

    deploy: github page settings

--- src/components/Socials/icons.jsx
+++
@@ -1,9 +0,0 @@
-import { Github } from "@styled-icons/boxicons-logos/Github"
-import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
-
-const Icons = {
-  Github,
-  Instagram,
-}
-
-export default Icons
