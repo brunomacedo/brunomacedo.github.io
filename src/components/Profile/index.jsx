7cde9031cd71546bce4c183093453ee7f67b90ac
Bruno Macedo <contato@brunomacedo.com.br>
2020-05-16 15:49:22 -0300

    fix: warning

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { useStaticQuery, graphql, Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Avatar from "../Avatar"
 import Socials from "../Socials"
